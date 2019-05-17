eac9dbb723cc50bdc90054750c5065ff693cbe5a
Animesh Lohar <kpsmiling92@gmail.com>
2019-05-17 11:35:05 +0530

    First commit

---
+++ SMSApi/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace SMSApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public bool Get(string to, string from, string message)
+        {
+            SendSMS s = new SendSMS();
+            return s.SMStsatus(to, from, message);
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SMSApi/SendSMS.cs
@@ -0,0 +1,36 @@
+﻿using SafetyAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using ASPSnippets.SmsAPI;
+
+
+namespace SMSApi
+{
+    public class SendSMS
+    {
+        public bool SMStsatus(string to, string from, string message)
+        {
+            string AccountSid = "AC38ace510c0bab7f8f9aa436e420a9c29";
+            string AuthToken = "80237d8a546b3b17b30b1c424d515491";
+            var twilio = new Twilio.TwilioRestClient(AccountSid, AuthToken);
+
+
+        //    var mess = twilio.SendMessage(
+        //    "+13477565536", "+917797073789",
+        //    "Hi Ishita..."
+        //);
+            var mess = twilio.SendMessage(
+                from, to,
+                message
+            );
+            if (mess != null)
+                return true;
+            else
+                return false;
+        }
+            
+    }
+}
